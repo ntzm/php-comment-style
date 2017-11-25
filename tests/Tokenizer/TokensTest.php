105bc883cee3057a0dc592a2a865d57254f8f9e8
Nat Zimmermann <nat@ntzm.io>
2017-11-25 08:48:01 +0000

    Add basic token test

---
+++ tests/Tokenizer/TokensTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ntzm\Tests\PhpCommentStyle;
+
+use Ntzm\PhpCommentStyle\Tokenizer\Tokens;
+use PHPUnit\Framework\TestCase;
+
+final class TokensTest extends TestCase
+{
+    public function testHasChanged(): void
+    {
+        $tokens = new Tokens(\token_get_all('<?php echo 1;'));
+
+        $this->assertFalse($tokens->hasChanged());
+
+        $tokens->replace(1, 'print');
+
+        $this->assertTrue($tokens->hasChanged());
+    }
+
+    public function testGetCode(): void
+    {
+        $tokens = new Tokens(\token_get_all('<?php echo 1;'));
+
+        $this->assertSame('<?php echo 1;', $tokens->toCode());
+
+        $tokens->replace(1, 'print');
+
+        $this->assertSame('<?php print 1;', $tokens->toCode());
+    }
+}
